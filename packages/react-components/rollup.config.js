91fea9b7d6c20ae86d8195036732e6b651e099f3
Mattia <astorino.mattia@gmail.com>
2022-01-13 13:25:54 +0100

    chore: init turborepo and builder. Wip

--- packages/react-components/rollup.config.js
+++ packages/react-components/rollup.config.js
@@ -6,7 +6,6 @@ import external from 'rollup-plugin-peer-deps-external'
 import postcss from 'rollup-plugin-postcss'
 import dts from 'rollup-plugin-dts'
 import svg from 'rollup-plugin-svg-sprite-loader'
-import alias from '@rollup/plugin-alias'
 import cleaner from 'rollup-plugin-cleaner'
 
 export default [
@@ -51,53 +50,3 @@ export default [
     plugins: [dts()]
   }
 ]
-
-// import commonjs from '@rollup/plugin-commonjs'
-// import typescript from '@rollup/plugin-typescript'
-// // import dts from 'rollup-plugin-dts'
-// import { terser } from 'rollup-plugin-terser'
-// import peerDepsExternal from 'rollup-plugin-peer-deps-external'
-// import json from '@rollup/plugin-json'
-// import postcss from 'rollup-plugin-postcss'
-// import cleaner from 'rollup-plugin-cleaner'
-// import svg from 'rollup-plugin-svg-sprite-loader'
-
-// export default [
-//   {
-//     input: 'src/index.ts',
-//     output: [
-//       {
-//         file: 'dist/index.js',
-//         format: 'cjs',
-//         sourcemap: false,
-//         exports: 'auto'
-//       }
-//     ],
-//     plugins: [
-//       cleaner({
-//         targets: [
-//           './dist/'
-//         ]
-//       }),
-//       peerDepsExternal(),
-//       postcss({
-//         extract: true,
-//         minimize: true
-//       }),
-//       commonjs(),
-//       json(),
-//       svg({
-//         extract: true,
-//         publicPath: './'
-//       }),
-//       typescript({ tsconfig: './tsconfig.json' }),
-//       terser()
-//     ],
-//     external: ['react', 'react-dom']
-//   }
-//   // {
-//   //   input: 'dist/types/index.d.ts',
-//   //   output: [{ file: 'dist/index.d.ts', format: 'esm' }],
-//   //   plugins: [dts()]
-//   // }
-// ]
