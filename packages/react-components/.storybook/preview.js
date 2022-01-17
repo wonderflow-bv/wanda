141a03c38496db87ffe43ac191b29d4eae3fd1d8
Mattia <astorino.mattia@gmail.com>
2022-01-17 11:42:52 +0100

    chore: update storybook settings and add themes

--- packages/react-components/.storybook/preview.js
+++ packages/react-components/.storybook/preview.js
@@ -1,13 +1,30 @@
-import './overrides.css';
+import '../dist/themes.css';
 import '../src/core.css';
+import './overrides.css';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-  viewMode: 'docs',
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
+  themes: {
+    default: 'light',
+    list: [
+      { name: 'auto', color: 'linear-gradient(to bottom right, lightgray 50%, black 50.1%)' },
+      { name: 'light', color: 'lightgray' },
+      { name: 'dark', color: 'black' }
+    ],
+    onChange: (theme) => {
+      const iframe = document.querySelector('#storybook-preview-iframe');
+      if (theme) {
+        iframe.contentDocument.documentElement.dataset.theme = theme.name
+      } else {
+        iframe.contentDocument.documentElement.dataset.theme = 'auto'
+      }
+    },
+    target: 'root'
+  }
 }
