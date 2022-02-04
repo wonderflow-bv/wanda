d332bb103cdddad04ca751c566743bd6b6a4d275
Mattia Astorino <equinusocio@users.noreply.github.com>
2022-02-04 11:41:44 +0100

    Feat/code highlight (#72)
    * refactor(code-block): clean and improve component
    
    * chore: clean codeblock code
    
    * docs(changeset): Update codeblock component
    
    * chore: fix code-block parsing

--- apps/docs/src/components/code-block/code-block.tsx
+++ apps/docs/src/components/code-block/code-block.tsx
@@ -1,86 +1,73 @@
 /* eslint-disable react/no-array-index-key */
 import { Button, Text, Stack } from '@wonderflow/react-components'
 import clsx from 'clsx'
-import Highlight, { defaultProps, Language } from 'prism-react-renderer'
 import rangeParser from 'parse-numeric-range'
-import React, { useCallback, useRef } from 'react'
+import Refractor from 'react-refractor'
+import React, { useCallback } from 'react'
 
-import { CodeBlock as CodeBlockClass, Action, Code, Toolbar, LineNumber, LineContent } from './code-block.module.css'
-import theme from './wonder-theme'
+// Load any languages you want to use from `refractor`
+import js from 'refractor/lang/javascript'
+import typescript from 'refractor/lang/typescript'
+import css from 'refractor/lang/css'
+import jsx from 'refractor/lang/jsx'
+import tsx from 'refractor/lang/tsx'
+import json from 'refractor/lang/json'
+import html from 'refractor/lang/markup'
+import bash from 'refractor/lang/bash'
+import diff from 'refractor/lang/diff'
+
+import { CodeBlock as CodeBlockClass, Action, Toolbar } from './code-block.module.css'
+
+Refractor.registerLanguage(js)
+Refractor.registerLanguage(css)
+Refractor.registerLanguage(jsx)
+Refractor.registerLanguage(tsx)
+Refractor.registerLanguage(json)
+Refractor.registerLanguage(html)
+Refractor.registerLanguage(typescript)
+Refractor.registerLanguage(bash)
+Refractor.registerLanguage(diff)
 
 type CodeBlockProps = {
   children: any;
   highlight?: string;
   hideCopy?: boolean;
-  showLineNumbers?: boolean;
 } & PropsWithClass
 
 export const CodeBlock: React.FC<CodeBlockProps> = ({
   children,
   highlight,
-  showLineNumbers = false,
   hideCopy = false,
   className
 }) => {
   const isNotString = typeof children !== 'string'
-  const language: Language = isNotString ? children?.props.className.replace(/(lang|language)-/g, '') : className?.replace(/(lang|language)-/g, '')
-  const CodeRef = useRef<any>('')
+  const language = isNotString ? children?.props.className.replace(/(lang|language)-/g, '') : className?.replace(/(lang|language)-/g, '')
 
   const copyContent = useCallback(
     () => () => {
-      navigator.clipboard.writeText(CodeRef.current.innerText)
+      navigator.clipboard.writeText(children)
     },
-    [CodeRef]
+    [children]
   )
 
   const formattedChildren = isNotString ? children.props.children.trim() : children.trim()
 
   return (
     <div className={clsx(CodeBlockClass)} data-code-block-has-highlight={Boolean(highlight)}>
-      <Highlight {...defaultProps} theme={theme} code={formattedChildren} language={language}>
-        {({
-          className, style, tokens, getLineProps, getTokenProps
-        }) => (
-          <>
-            <pre ref={CodeRef} className={clsx(Code, className)} style={{ ...style }}>
-              {tokens.map((line, i) => (
-                // Line
-                <div
-                  key={i}
-                  style={{ display: 'table-row' }}
-                  data-code-block-highlight={highlight && rangeParser(highlight).includes(i + 1)}
-                  {...getLineProps({ line, key: i })}
-                >
-                  {/* Line number */}
-                  {showLineNumbers && (
-                    <span className={LineNumber}>
-                      {i + 1}
-                    </span>
-                  )}
-                  <span className={LineContent}>
-                    {line.map((token, key) => (
-                      <span key={key} {...getTokenProps({ token, key })} />
-                    ))}
-                  </span>
-                </div>
-              ))}
-            </pre>
-            <Stack direction="row" fill={false} horizontalAlign="space-between" verticalAlign="center" className={Toolbar}>
-              {language && <Text responsive={false} size={14} dimmed={5}>{language}</Text>}
-              {!hideCopy && (
-                <Button
-                  className={Action}
-                  dimension="small"
-                  kind="flat"
-                  onClick={copyContent()}
-                >
-                  Copy
-                </Button>
-              )}
-            </Stack>
-          </>
+      <Refractor language={language} value={formattedChildren} markers={highlight ? rangeParser(highlight) : undefined} />
+      <Stack direction="row" fill={false} horizontalAlign="space-between" verticalAlign="center" className={Toolbar}>
+        {language && <Text responsive={false} size={14} dimmed={5}>{language}</Text>}
+        {!hideCopy && (
+        <Button
+          className={Action}
+          dimension="small"
+          kind="flat"
+          onClick={copyContent()}
+        >
+          Copy
+        </Button>
         )}
-      </Highlight>
+      </Stack>
     </div>
   )
 }
