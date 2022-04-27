44b6eb30a3ac405f2a538a6bfbe604ee737db311
Mattia <astorino.mattia@gmail.com>
2022-04-27 14:46:35 +0200

    chore: update timing

--- apps/docs/src/examples/advanced-motion/advanced-motion.tsx
+++ apps/docs/src/examples/advanced-motion/advanced-motion.tsx
@@ -3,7 +3,7 @@ import styles from './advanced-motion.module.css'
 
 export const AdvancedMotion = () => {
   const x = useMotionValue(0)
-  const xInput = [-100, 0, 100]
+  const xInput = [-200, 0, 200]
   const background = useTransform(x, xInput, [
     '#ff0105',
     'hsl(0 100% 80% / 0%)',
@@ -11,7 +11,7 @@ export const AdvancedMotion = () => {
   ])
   const color = useTransform(x, xInput, [
     '#e1090d',
-    '#2600ff',
+    '#7f7f7f',
     '#00d856'
   ])
   const tickPath = useTransform(x, [10, 100], [0, 1])
@@ -26,8 +26,14 @@ export const AdvancedMotion = () => {
         className={styles.Box}
         style={{ x }}
         drag="x"
-        dragElastic={0.15}
+        dragElastic={0.5}
+        whileTap={{ scale: 1.1 }}
         dragConstraints={{ left: 0, right: 0 }}
+        transition={{
+          type: 'spring',
+          stiffness: 700,
+          damping: 20
+        }}
       >
         <svg className={styles.ProgressIcon} viewBox="0 0 50 50">
           <motion.path
