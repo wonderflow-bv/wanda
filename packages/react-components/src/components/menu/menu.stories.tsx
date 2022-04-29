c44582682fbf37fddefeba5c993cfd929dda7363
Mattia <astorino.mattia@gmail.com>
2022-04-29 12:22:20 +0200

    feat: add Menu.Separator element

--- packages/react-components/src/components/menu/menu.stories.tsx
+++ packages/react-components/src/components/menu/menu.stories.tsx
@@ -1,7 +1,7 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react'
 import { useState } from 'react'
 import { Menu } from './menu'
-import { Separator, Chip } from '../..'
+import { Chip } from '../..'
 
 const story: ComponentMeta<typeof Menu> = {
   title: 'Components/Navigation/Menu',
@@ -15,7 +15,7 @@ const Template: ComponentStory<typeof Menu> = (args) => (
     <Menu.Item value="1" padding={false}>List item text</Menu.Item>
     <Menu.Item value="2" padding={false}>List item text</Menu.Item>
     <Menu.Item value="3" padding={false}>List item text List item textList item textList item textList item text</Menu.Item>
-    <Separator />
+    <Menu.Separator />
     <Menu.Item value="4" padding={false}>List item text</Menu.Item>
   </Menu>
 )
@@ -31,7 +31,7 @@ const WithIconsTemplate: ComponentStory<typeof Menu> = (args) => (
   <Menu {...args}>
     <Menu.Item value="1" icon="user">List item text</Menu.Item>
     <Menu.Item value="2" icon="message">List item text List item</Menu.Item>
-    <Separator />
+    <Menu.Separator />
     <Menu.Item value="3">List item text</Menu.Item>
     <Menu.Item value="4" icon="bell">List item text</Menu.Item>
   </Menu>
@@ -69,7 +69,7 @@ const LinksTemplate: ComponentStory<typeof Menu> = (args) => (
   <Menu {...args}>
     <Menu.Item value="1" as="a" href="https://design.wonderflow.ai" icon="user">List item text</Menu.Item>
     <Menu.Item value="2" icon="message">List item text List item</Menu.Item>
-    <Separator />
+    <Menu.Separator />
     <Menu.Item value="3">List item text</Menu.Item>
     <Menu.Item value="4" as="a" href="https://design.wonderflow.ai" icon="bell">List item text</Menu.Item>
   </Menu>
