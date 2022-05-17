011a4dfd81aa37c70c9af9714d8251c493af292e
Mattia <astorino.mattia@gmail.com>
2022-05-17 11:55:40 +0200

    chore: update meta

--- apps/doc/pages/_app.tsx
+++ apps/doc/pages/_app.tsx
@@ -1,10 +1,55 @@
 import '@wonderflow/themes';
 import '@wonderflow/react-components/core.css';
 
+import { AnimatePresence } from 'framer-motion';
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import NextScript from 'next/script';
+import NextProgress from 'next-progress';
+import { ThemeProvider } from 'next-themes';
+import { useEffect } from 'react';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+const Providers: FCChildren = ({ children }) => (
+  <ThemeProvider defaultTheme="system" disableTransitionOnChange>
+    {/* <IdProvider> */}
+    {children}
+    {/* </IdProvider> */}
+  </ThemeProvider>
+);
 
-export default MyApp;
+const App = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+
+  const setScrollSmooth = (value: boolean) => {
+    document.documentElement.dataset.htmlSmooth = String(value);
+  };
+
+  useEffect(() => {
+    const handleSmoothScroll = (val: boolean) => () => {
+      setScrollSmooth(val);
+    };
+
+    router.events.on('routeChangeStart', handleSmoothScroll(false));
+    router.events.on('routeChangeComplete', handleSmoothScroll(true));
+    router.events.on('routeChangeError', handleSmoothScroll(true));
+
+    return () => {
+      router.events.off('routeChangeStart', handleSmoothScroll(false));
+      router.events.off('routeChangeComplete', handleSmoothScroll(true));
+      router.events.off('routeChangeError', handleSmoothScroll(true));
+    };
+  }, [router]);
+
+  return (
+    <Providers>
+      <NextScript id="docsearch-core" src="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.js" strategy="beforeInteractive" />
+      <NextProgress delay={300} color="var(--cta-default)" options={{ showSpinner: false, minimum: 0.3 }} />
+      <AnimatePresence exitBeforeEnter initial={false}>
+        <Component {...pageProps} />
+      </AnimatePresence>
+    </Providers>
+  );
+};
+
+export default App;
+
