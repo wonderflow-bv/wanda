14d78e548af301f9970c048863e485dc36ecdcc2
Mattia <astorino.mattia@gmail.com>
2022-05-19 10:52:10 +0200

    chore: add header

---
+++ apps/docs/src/components/shared/search/search.tsx
@@ -0,0 +1,26 @@
+import { DocSearchProps } from '@docsearch/react';
+import { Skeleton } from '@wonderflow/react-components';
+import dynamic from 'next/dynamic';
+import Head from 'next/head';
+
+const DynDocSearch = dynamic<DocSearchProps>(
+  async () => import('@docsearch/react').then(m => m.DocSearch),
+  {
+    ssr: false,
+    loading: () => <Skeleton height={24} width={150} />,
+  },
+);
+
+export const Search = () => (
+  <>
+    <Head>
+      <link rel="preconnect" href="https://BR3WX4TQPD-dsn.algolia.net" crossOrigin="anonymous" />
+    </Head>
+    <DynDocSearch
+      appId="BR3WX4TQPD"
+      apiKey={process.env.NEXT_PUBLIC_ALGOLIA_API_KEY}
+      indexName="wonderflow"
+      placeholder="Search content across the website..."
+    />
+  </>
+);
