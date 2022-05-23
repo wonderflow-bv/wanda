1df392fa743e18e6376d821668a7a927f403ddf4
Mattia <astorino.mattia@gmail.com>
2022-05-23 12:07:53 +0200

    chore: update colors card

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -1,17 +1,22 @@
 import type { NextPage } from 'next';
+import dynamic from 'next/dynamic';
 
-import { Designers } from '@/components/home/designers';
 import { Hero } from '@/components/home/hero';
 import { Mentions } from '@/components/home/mentions';
 import { Meta } from '@/components/shared/meta';
 import { getPageStaticProps } from '@/core/get-page-props';
+
+const DynDesigners = dynamic<Record<string, any>>(
+  async () => import('@/components/home/designers').then(mod => mod.Designers),
+  { ssr: false },
+);
 
 const Home: NextPage = () => (
   <>
     <Meta />
     <Hero />
     <Mentions />
-    <Designers />
+    <DynDesigners />
   </>
 );
 
