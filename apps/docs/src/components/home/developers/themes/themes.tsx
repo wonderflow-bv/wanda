358bf9b1c8a62942dd50a4b786f98c30a09094ee
Mattia <astorino.mattia@gmail.com>
2022-05-27 17:44:26 +0200

    chore: update themes

--- apps/docs/src/components/home/developers/themes/themes.tsx
+++ apps/docs/src/components/home/developers/themes/themes.tsx
@@ -1,26 +1,34 @@
 import {
-  Elevator, Masonry,
+  Elevator, Masonry, Stack,
 } from '@wonderflow/react-components';
 
 import { Browser } from '@/components/shared/browser';
 
+import styles from './themes.module.css';
+
 export const Themes = () => (
   <Elevator resting={4}>
-    <Browser>
-      <Masonry
-        columns={{
-          default: 3,
-          small: 3,
-        }}
-      >
-        <img alt="" src="https://picsum.photos/200/300" />
-        <img alt="" src="https://picsum.photos/200/300" />
-        <img alt="" src="https://picsum.photos/200/300" />
-        <img alt="" src="https://picsum.photos/200/300" />
-        <img alt="" src="https://picsum.photos/200/300" />
-        <img alt="" src="https://picsum.photos/200/300" />
-        <img alt="" src="https://picsum.photos/200/300" />
-      </Masonry>
+    <Browser className={styles.Themes}>
+      <Stack hPadding={16} vPadding={16}>
+        <Masonry
+          columns={{
+            default: 3,
+            small: 3,
+          }}
+        >
+          <img alt="" width="122" loading="lazy" decoding="async" src="https://images.unsplash.com/photo-1652298756973-b448d256fe6c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=244&q=80" />
+          <img alt="" width="122" loading="lazy" decoding="async" src="https://images.unsplash.com/photo-1635792805734-4bb1f8ade575?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=244&q=80" />
+          <img alt="" width="122" loading="lazy" decoding="async" src="https://images.unsplash.com/photo-1592766472014-693f057233d3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=244&q=80" />
+          <img alt="" width="122" loading="lazy" decoding="async" src="https://images.unsplash.com/photo-1598332601704-e1b27cce3370?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=244&q=80" />
+          <img alt="" width="122" loading="lazy" decoding="async" src="https://images.unsplash.com/photo-1652045143118-52f408271feb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=244&q=80" />
+          <img alt="" width="122" loading="lazy" decoding="async" src="https://images.unsplash.com/photo-1613370546003-ddc7faae1779?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=244&q=80" />
+          <img alt="" width="122" loading="lazy" decoding="async" src="https://images.unsplash.com/photo-1652045681043-34953d24892b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=244&q=80" />
+          <img alt="" width="122" loading="lazy" decoding="async" src="https://images.unsplash.com/photo-1635663684911-6b1a5d5affb9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=244&q=80" />
+          <img alt="" width="122" loading="lazy" decoding="async" src="https://images.unsplash.com/photo-1653203068247-f9e97de773db?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=244&q=80" />
+          <img alt="" width="122" loading="lazy" decoding="async" src="https://images.unsplash.com/photo-1653202916210-9e41f0173b6b?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=244" />
+          <img alt="" width="122" loading="lazy" decoding="async" src="https://images.unsplash.com/photo-1635752038829-b2095d6bd8c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=244&q=80" />
+        </Masonry>
+      </Stack>
     </Browser>
   </Elevator>
 );
