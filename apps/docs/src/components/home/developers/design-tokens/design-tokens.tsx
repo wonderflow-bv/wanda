fff893d0813e216d83bb05c576c078c114ab567a
Mattia <astorino.mattia@gmail.com>
2022-05-27 09:50:21 +0200

    chore: update tokens

--- apps/docs/src/components/home/developers/design-tokens/design-tokens.tsx
+++ apps/docs/src/components/home/developers/design-tokens/design-tokens.tsx
@@ -1,6 +1,8 @@
 import { Elevator } from '@wonderflow/react-components';
 import tkns from '@wonderflow/tokens/platforms/web/tokens.json';
-import { useEffect, useState } from 'react';
+import {
+  useEffect, useMemo, useRef,
+} from 'react';
 import { MarkerProps } from 'react-refractor';
 
 import { Code } from '@/components/shared/code';
@@ -8,30 +10,29 @@ import { Code } from '@/components/shared/code';
 import styles from './design-tokens.module.css';
 
 const skipGroup = 3;
-let startLine = 2;
 
 export const DesignTokens = () => {
-  const [colorTokens, setColorTokens] = useState<MarkerProps['markers']>();
+  const startLine = useRef(2);
 
-  useEffect(() => {
-    const arrayOfLines = Object.keys(tkns.color).map(
-      (color, index) => Object.keys(tkns.color[color]).map((colorKey, indexKey) => {
-        const addLine = index > 0 && indexKey === 0 ? skipGroup : 1;
-        startLine += addLine;
-        return ({
-          line: startLine,
-          component: (props: MarkerProps) => (
-            <span className={styles.Row}>
-              {props.children}
-              <span className={styles.ColorPreview} style={{ background: `hsl(${tkns.color[color][colorKey]})` }} />
-            </span>
-          ),
-        });
-      }),
-    ).flat();
+  const arrayOfLines = useMemo(() => Object.keys(tkns.color).map(
+    (color, index) => Object.keys(tkns.color[color]).map((colorKey, indexKey) => {
+      const addLine = index > 0 && indexKey === 0 ? skipGroup : 1;
+      startLine.current += addLine;
+      return ({
+        line: startLine.current,
+        component: (props: MarkerProps) => (
+          <span className={styles.Row}>
+            {props.children}
+            <span className={styles.ColorPreview} style={{ background: `hsl(${tkns.color[color][colorKey]})` }} />
+          </span>
+        ),
+      });
+    }),
+  ).flat(), []);
 
-    setColorTokens(arrayOfLines);
-  }, []);
+  useEffect(() => () => {
+    startLine.current = 2;
+  }, [arrayOfLines]);
 
   return (
     <div>
@@ -40,7 +41,7 @@ export const DesignTokens = () => {
           language="json"
           maxHeight="25rem"
           source={JSON.stringify(tkns.color, null, 3)}
-          markers={colorTokens}
+          markers={arrayOfLines}
         />
       </Elevator>
     </div>
