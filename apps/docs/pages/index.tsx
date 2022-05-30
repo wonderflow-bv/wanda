7667e46e8190fbc84882fdc74c76b6d305761798
Mattia <astorino.mattia@gmail.com>
2022-05-30 10:54:49 +0200

    chore: update layoutprops

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -4,7 +4,7 @@ import dynamic from 'next/dynamic';
 import { Hero } from '@/components/home/hero';
 import { Mentions } from '@/components/home/mentions';
 import { Meta } from '@/components/shared/meta';
-import { getPageStaticProps } from '@/core/get-page-props';
+import { getLayoutProps } from '@/core/get-page-props';
 
 const DynDesigners = dynamic<Record<string, any>>(
   async () => import('@/components/home/designers').then(mod => mod.Designers),
@@ -27,9 +27,10 @@ const Home: NextPage = () => (
 
 export default Home;
 
-export const getStaticProps = getPageStaticProps({
+export const getStaticProps = () => getLayoutProps({
   layout: 'blank',
   layoutProps: {
     headerPosition: 'absolute',
   },
 });
+
