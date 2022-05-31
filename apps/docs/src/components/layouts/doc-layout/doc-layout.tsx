31406a980d9aefc3295f43f884b52d1dca35ee12
Mattia <astorino.mattia@gmail.com>
2022-05-31 18:07:30 +0200

    chore: update nav

--- apps/docs/src/components/layouts/doc-layout/doc-layout.tsx
+++ apps/docs/src/components/layouts/doc-layout/doc-layout.tsx
@@ -1,11 +1,16 @@
-import { Container, Stack } from '@wonderflow/react-components';
+import {
+  Container, Icon, Stack, Text,
+} from '@wonderflow/react-components';
 import clsx from 'clsx';
 import dynamic from 'next/dynamic';
-import { HTMLAttributes, PropsWithChildren } from 'react';
+import Link from 'next/link';
+import { NextRouter, useRouter } from 'next/router';
+import { HTMLAttributes, PropsWithChildren, useCallback } from 'react';
 
 import { BaseLayout } from '@/components/layouts/base-layout';
 import { HeaderProps } from '@/components/shared/header';
 import { useResponsiveContext } from '@/context/responsive';
+import DocNav from '@/data/doc-nav';
 
 import styles from './doc-layout.module.css';
 
@@ -20,20 +25,53 @@ const DynHeader = dynamic<HeaderProps>(
 
 export const DocLayout: FCChildren<IPropsDocLayout> = ({ children }) => {
   const { matches } = useResponsiveContext();
+  const router = useRouter();
+
+  const includesPath = useCallback(
+    (path: NextRouter['asPath']) => {
+      const matchURL = router.asPath.split('/')[1] === path;
+      return matchURL && router.asPath.split('/')[1].length > 0;
+    },
+    [router.asPath],
+  );
 
   return (
     <BaseLayout>
       <DynHeader position="sticky" />
       <Container dimension="large">
         <Stack direction={matches.large ? 'row' : undefined} columnGap={56}>
+
           <div className={styles.Sidebar}>
-            <Stack>
-              SIDEBAR
+            <Stack rowGap={40}>
+              <Stack rowGap={8}>
+                {DocNav.map(link => (
+                  <Link href={link.url} passHref>
+                    <Stack
+                      as="a"
+                      className={styles.NavItem}
+                      style={{ '--bg': `var(--highlight-${link.color}-background)`, '--fg': `var(--highlight-${link.color}-foreground)` }}
+                      direction="row"
+                      columnGap={8}
+                      hAlign="start"
+                      vAlign="center"
+                      fill={false}
+                      aria-current={includesPath(link.url.split('/')[1]) ? 'true' : undefined}
+                    >
+                      <Stack as="span" hAlign="center" vAlign="center" className={styles.IconWrapper}>
+                        <Icon source={link.icon} dimension={18} weight="duotone" />
+                      </Stack>
+                      <Text as="span" weight="bold" size={16}>{link.label}</Text>
+                    </Stack>
+                  </Link>
+                ))}
+              </Stack>
             </Stack>
           </div>
+
           <main className={styles.Content}>
             {children}
           </main>
+
           <div className={clsx(styles.Sidebar, styles.Toc)}>toc</div>
         </Stack>
       </Container>
