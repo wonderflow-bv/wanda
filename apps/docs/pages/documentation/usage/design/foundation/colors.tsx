09f13bce83a0511626124d2a6d5ececc5a801956
Mattia <astorino.mattia@gmail.com>
2022-06-06 09:05:28 +0200

    chore: update layouts and fix some css

--- apps/docs/pages/documentation/usage/design/foundation/colors.tsx
+++ apps/docs/pages/documentation/usage/design/foundation/colors.tsx
@@ -1,6 +1,6 @@
 import { NextPage } from 'next';
 
-import { getLayoutProps } from '@/core/get-page-props';
+import { getLayoutProps } from '@/core/get-layout-props';
 
 const ColorsPage: NextPage = () => (<div>Color</div>);
 
