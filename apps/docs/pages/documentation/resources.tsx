67a7accd01a7889b990eeca849ab247bfcfa62bf
Mattia <astorino.mattia@gmail.com>
2022-06-09 16:49:47 +0200

    feat: add Resource page

--- apps/docs/pages/documentation/resources.tsx
+++ apps/docs/pages/documentation/resources.tsx
@@ -1,8 +1,44 @@
+import { IconNames } from '@wonderflow/icons';
+import { Grid, Stack, Title } from '@wonderflow/react-components';
 import { NextPage } from 'next';
+import { useResourcesQuery } from 'src/generated/graphql';
 
+import { Tile } from '@/components/shared/tile';
 import { getLayoutProps } from '@/utils/get-layout-props';
 
-const ResourcesPage: NextPage = () => (<div>Resources</div>);
+const ResourcesPage: NextPage = () => {
+  const { data, loading } = useResourcesQuery();
+
+  return loading
+    ? <>Loading...</>
+    : (
+      <Stack rowGap={80} vPadding={56}>
+        {data?.resourceGroups.map(group => (
+          <Stack as="section" rowGap={24} key={group.id}>
+            <Title as="h2" level="5">{group.groupName}</Title>
+            <Grid
+              columns={2}
+              colMinWidth="20rem"
+              columnGap={4}
+              rowGap={4}
+            >
+              {group.resources?.map(res => (
+                <Grid.Item key={res.title}>
+                  <Tile
+                    title={res.title}
+                    description={res.description}
+                    symbol={group.groupSymbol as IconNames}
+                    symbolColor={group.groupColor}
+                    url={res.url}
+                  />
+                </Grid.Item>
+              ))}
+            </Grid>
+          </Stack>
+        ))}
+      </Stack>
+    );
+};
 
 export default ResourcesPage;
 
