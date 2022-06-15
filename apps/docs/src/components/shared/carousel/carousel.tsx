cfa2e381e7123ab0f1c135c7b3cbc689ad32dc2f
Mattia <astorino.mattia@gmail.com>
2022-06-15 10:54:37 +0200

    chore: update home

--- apps/docs/src/components/shared/carousel/carousel.tsx
+++ apps/docs/src/components/shared/carousel/carousel.tsx
@@ -2,31 +2,26 @@ import { Container } from '@wonderflow/react-components';
 import React from 'react';
 import Marquee from 'react-fast-marquee';
 
-import { useResponsive } from '@/context/responsive';
-
 import styles from './carousel.module.css';
 
 type CarouselProps = {
   speed?: number;
+  fade?: boolean;
 }
 
 export const Carousel: FCChildrenClass<CarouselProps> = ({
   children,
   speed,
+  fade,
   ...otherProps
-}) => {
-  const { matches } = useResponsive();
-
-  return (
-    <Container
-      className={styles.Carousel}
-      data-carousel-fade={matches.extraLarge}
-      dimension={matches.extraLarge ? 'large' : 'full'}
-      padding={!matches.extraLarge ? false : undefined}
-    >
-      <Marquee speed={speed} gradient={false} className={styles.Slider} {...otherProps}>
-        {children}
-      </Marquee>
-    </Container>
-  );
-};
+}) => (
+  <Container
+    className={styles.Carousel}
+    data-carousel-fade={fade}
+    padding={false}
+  >
+    <Marquee speed={speed} gradient={false} className={styles.Slider} {...otherProps}>
+      {children}
+    </Marquee>
+  </Container>
+);
