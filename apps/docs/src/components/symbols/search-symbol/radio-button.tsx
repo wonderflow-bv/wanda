b7eac60dcdef69691fbcc8f5d0778f89a2c1890d
Mattia <astorino.mattia@gmail.com>
2022-06-17 15:31:13 +0200

    chore: remove tooltip to restore keyboard navigation

--- apps/docs/src/components/symbols/search-symbol/radio-button.tsx
+++ apps/docs/src/components/symbols/search-symbol/radio-button.tsx
@@ -1,6 +1,4 @@
-import {
-  Stack, Symbol, SymbolProps, Tooltip,
-} from '@wonderflow/react-components';
+import { Stack, Symbol, SymbolProps } from '@wonderflow/react-components';
 import { ReactElement } from 'react';
 
 import styles from './search-symbol.module.css';
@@ -18,39 +16,30 @@ export const RadioButton = ({
   checked,
   onClick,
 }: RadioButtonProps) => (
-  <Tooltip trigger={(
-    <Stack
-      as="label"
-      fill={false}
-      htmlFor={`${weight ?? 'No'}Style`}
-      className={styles.RadioButton}
-      data-checked={checked}
-      vAlign="center"
-      hAlign="center"
-      direction="row"
-      columnGap={16}
-      vPadding={16}
-      hPadding={16}
-    >
-      <input
-        type="radio"
-        onChange={onClick}
-        id={`${weight ?? 'No'}Style`}
-        name="iconstyle"
-        value="solid"
-        defaultChecked={checked}
-        className={styles.Input}
-      />
-      <Symbol
-        source={children ?? 'disc'}
-      />
-    </Stack>
-)}
+  <Stack
+    as="label"
+    fill={false}
+    htmlFor={`${weight ?? 'No'}Style`}
+    className={styles.RadioButton}
+    data-checked={checked}
+    vAlign="center"
+    hAlign="center"
+    direction="row"
+    columnGap={16}
+    vPadding={16}
+    hPadding={16}
   >
-    Set
-    {' '}
-    {weight}
-    {' '}
-    style
-  </Tooltip>
+    <input
+      type="radio"
+      onChange={onClick}
+      id={`${weight ?? 'No'}Style`}
+      name="iconstyle"
+      value="solid"
+      defaultChecked={checked}
+      className={styles.Input}
+    />
+    <Symbol
+      source={children ?? 'disc'}
+    />
+  </Stack>
 );
