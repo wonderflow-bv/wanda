42205c4738aa5bce013d4ab123b92b35b74b7652
Mattia <astorino.mattia@gmail.com>
2022-06-17 14:51:15 +0200

    chore: update Symbols page

---
+++ apps/docs/src/components/symbols/search-symbol/radio-button.tsx
@@ -0,0 +1,56 @@
+import {
+  Stack, Symbol, SymbolProps, Tooltip,
+} from '@wonderflow/react-components';
+import { ReactElement } from 'react';
+
+import styles from './search-symbol.module.css';
+
+type RadioButtonProps = {
+  children: ReactElement<HTMLOrSVGElement>;
+  weight: SymbolProps['weight'];
+  checked?: boolean;
+  onClick: () => void;
+}
+
+export const RadioButton = ({
+  children,
+  weight,
+  checked,
+  onClick,
+}: RadioButtonProps) => (
+  <Tooltip trigger={(
+    <Stack
+      as="label"
+      fill={false}
+      htmlFor={`${weight ?? 'No'}Style`}
+      className={styles.RadioButton}
+      data-checked={checked}
+      vAlign="center"
+      hAlign="center"
+      direction="row"
+      columnGap={16}
+      vPadding={16}
+      hPadding={16}
+    >
+      <input
+        type="radio"
+        onChange={onClick}
+        id={`${weight ?? 'No'}Style`}
+        name="iconstyle"
+        value="solid"
+        defaultChecked={checked}
+        className={styles.Input}
+      />
+      <Symbol
+        source={children ?? 'disc'}
+      />
+    </Stack>
+)}
+  >
+    Set
+    {' '}
+    {weight}
+    {' '}
+    style
+  </Tooltip>
+);
