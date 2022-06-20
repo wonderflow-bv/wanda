2888197acd0b23ca7771609841a8fd9d2ba6941b
Mattia <astorino.mattia@gmail.com>
2022-06-20 17:02:56 +0200

    chore: add globe

--- apps/docs/src/components/shared/brands/brands.tsx
+++ apps/docs/src/components/shared/brands/brands.tsx
@@ -1,9 +1,10 @@
 import {
   Container, Stack, Title,
 } from '@wonderflow/react-components';
+import createGlobe from 'cobe';
 import Image from 'next/image';
 import { useTheme } from 'next-themes';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import { ClientOnly } from '@/components/shared/client-only';
 import { GradientText } from '@/components/shared/gradient-text';
@@ -24,6 +25,7 @@ export const Brands = ({
 }) => {
   const [isDark, setIsDark] = useState<boolean>(false);
   const { theme } = useTheme();
+  const canvasRef = useRef<HTMLCanvasElement>(null);
 
   const mediaMatches = (matches: boolean) => {
     setIsDark(matches);
@@ -37,6 +39,52 @@ export const Brands = ({
       window.matchMedia('(prefers-color-scheme: dark)').removeEventListener('change', ({ matches }) => mediaMatches(matches));
     };
   }, [theme]);
+
+  useEffect(() => {
+    const darkFlag = theme === 'dark' ? 1 : 0;
+    const darkGlobeBg: [number, number, number] = theme === 'dark' ? [0.3, 0.3, 0.3] : [1, 1, 1];
+    const globeBackground: [number, number, number] = (theme === 'system' && isDark) ? [0.3, 0.3, 0.3] : [1, 1, 1];
+
+    let rotation = 0;
+    let globe: any = null;
+
+    if (canvasRef.current) {
+      globe = createGlobe(canvasRef.current, {
+        devicePixelRatio: 2,
+        width: 1000 * 2,
+        height: 1000 * 2,
+        phi: 0,
+        theta: 0,
+        dark: darkFlag,
+        diffuse: 1.2,
+        mapSamples: 26000,
+        mapBrightness: 3,
+        baseColor: theme === 'system' ? globeBackground : darkGlobeBg,
+        markerColor: [0.933, 0.0, 0.0],
+        glowColor: [1, 1, 1],
+        scale: 1,
+        offset: [0, 0],
+        markers: [
+          { location: [41.008240, 28.978359], size: 0.04 },
+          { location: [50.0679680, 8.508600], size: 0.04 },
+          { location: [52.370216, 4.895168], size: 0.04 },
+          { location: [45.666279, 12.242070], size: 0.04 },
+          { location: [59.329323, 18.068581], size: 0.04 },
+          { location: [46.5218269, 6.6327025], size: 0.04 },
+          { location: [37.7790262, -122.419906], size: 0.04 },
+        ],
+        onRender: (state) => {
+          // eslint-disable-next-line no-param-reassign
+          state.phi = rotation;
+          rotation -= 1 / 1000;
+        },
+      });
+    }
+
+    return () => {
+      globe.destroy();
+    };
+  }, [isDark, theme]);
 
   const gumletLoader = ({
     src,
@@ -52,11 +100,12 @@ export const Brands = ({
   return (
     <Section
       fill={false}
-      vAlign="center"
-      minHeight="15rem"
+      vAlign="start"
+      minHeight="25rem"
       className={styles.Brands}
       {...otherProps}
     >
+      <canvas ref={canvasRef} className={styles.Globe} />
       <Container as={Stack} hAlign="center" vAlign="center" dimension="large">
         <Title level="6">Some Wonderflow’s customers enjoying Wanda</Title>
         <Stack className={styles.Logos} direction="row" fill={false} hAlign="center" wrap>
