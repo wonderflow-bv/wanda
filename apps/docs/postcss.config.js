95008c10de320860037f1c96bb15f2af2990c1ac
Mattia <astorino.mattia@gmail.com>
2022-07-04 11:26:23 +0200

    chore: cose

--- apps/docs/postcss.config.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires, import/no-extraneous-dependencies */
-
-const { postcssConfig } = require('@wonderflow/config');
-
-module.exports = postcssConfig;
