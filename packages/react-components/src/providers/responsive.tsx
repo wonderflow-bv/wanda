d667561e53d65438e8e3056393cccfac944d2e23
Mattia <astorino.mattia@gmail.com>
2022-08-09 14:59:40 +0200

    fix: refactoring ResponsivePRovider

--- packages/react-components/src/providers/responsive.tsx
+++ packages/react-components/src/providers/responsive.tsx
@@ -1,81 +1,74 @@
-import { configResponsive, useResponsive as responsiveAhook } from 'ahooks';
-import {
-  createContext, useContext, useEffect, useState,
-} from 'react';
+/* eslint-disable @typescript-eslint/prefer-reduce-type-parameter */
+import jsonTokens from '@wonderflow/tokens/platforms/web/tokens.json';
+import constate from 'constate';
+import { useEffect, useState } from 'react';
 
-type ResponsiveContextProps = {
-  /**
-   * Current breakpoints
-  */
-  breakpoints: {
-    extraSmall: number;
-    small: number;
-    medium: number;
-    large: number;
-    extraLarge: number;
-  };
-  /**
-   * Object with breakpoints matching the current screen size.
-  */
-  matches: Record<keyof ResponsiveContextProps['breakpoints'], boolean>;
-}
-
-type ResponsiveProviderProps = {
-  breakpoints?: ResponsiveContextProps['breakpoints'];
-}
-
-const DEFAULT_BREAKPOINTS = {
-  extraSmall: 480,
-  small: 768,
-  medium: 960,
-  large: 1280,
-  extraLarge: 1600,
+type Breakpoints = {
+  extraSmall: string;
+  small: string;
+  medium: string;
+  large: string;
+  extraLarge: string;
 };
 
-export const ResponsiveContext = createContext<ResponsiveContextProps>({
-  breakpoints: DEFAULT_BREAKPOINTS,
-  matches: {
-    extraSmall: false,
-    small: false,
-    medium: false,
-    large: false,
-    extraLarge: false,
-  },
-});
+type BreakpointsKeys = keyof Breakpoints;
 
-ResponsiveContext.displayName = 'ResponsiveContext';
+const DEFAULT_BREAKPOINTS: Breakpoints = {
+  extraSmall: jsonTokens.breakpoint['extra-small'],
+  small: jsonTokens.breakpoint.small,
+  medium: jsonTokens.breakpoint.medium,
+  large: jsonTokens.breakpoint.large,
+  extraLarge: jsonTokens.breakpoint['extra-large'],
+};
 
-export const ResponsiveProvider: FCChildren<ResponsiveProviderProps> = ({
-  children,
-  breakpoints = DEFAULT_BREAKPOINTS,
-}) => {
-  configResponsive(breakpoints);
-  const responsiveHook = responsiveAhook();
-  const [matching, setMatching] = useState<ResponsiveContextProps['matches']>({
-    extraSmall: false,
-    small: false,
-    medium: false,
-    large: false,
-    extraLarge: false,
+const breakpointKeys = Object.keys(DEFAULT_BREAKPOINTS) as BreakpointsKeys[];
+
+const DEFAULT_BREAKPOINTS_MATCHES: Record<BreakpointsKeys, boolean> = {
+  extraSmall: false,
+  small: false,
+  medium: false,
+  large: false,
+  extraLarge: false,
+};
+
+const matchMediaFactory = (breakpointKey: BreakpointsKeys) => window.matchMedia(`(min-width: ${DEFAULT_BREAKPOINTS[breakpointKey]})`);
+
+const useResponsive = () => {
+  const [matches, setMatches] = useState<Record<BreakpointsKeys, boolean>>(() => {
+    if (typeof window === 'undefined') {
+      return DEFAULT_BREAKPOINTS_MATCHES;
+    }
+
+    const results = breakpointKeys.reduce((acc, src) => {
+      acc[src] = matchMediaFactory(src).matches;
+      return acc;
+    }, {} as Record<BreakpointsKeys, boolean>);
+
+    return results;
   });
 
   useEffect(() => {
-    setMatching(responsiveHook);
-  }, [breakpoints, responsiveHook]);
+    const onChange = (
+      key: BreakpointsKeys,
+    ) => (event: MediaQueryListEvent) => setMatches(old => ({ ...old, [key]: event.matches }));
 
-  return (
-    <ResponsiveContext.Provider value={{ breakpoints, matches: matching }}>
-      {children}
-    </ResponsiveContext.Provider>
-  );
+    const mediaQueryList = breakpointKeys.reduce((acc, src) => {
+      acc[src] = matchMediaFactory(src);
+      acc[src].addEventListener('change', onChange(src));
+      return acc;
+    }, {} as Record<BreakpointsKeys, MediaQueryList>);
+
+    return () => {
+      (Object.keys(mediaQueryList) as BreakpointsKeys[]).forEach(
+        src => mediaQueryList[src].removeEventListener('change', onChange(src)),
+      );
+    };
+  }, []);
+
+  return { matches, breakpoints: DEFAULT_BREAKPOINTS };
 };
 
-export const useResponsiveContext = () => {
-  const context = useContext(ResponsiveContext);
-  if (!context) {
-    throw new Error(
-      'useResponsiveContext hook must be used inside ResponsiveProvider to access context data.',
-    );
-  }
-  return context;
-};
+export const [
+  ResponsiveProvider,
+  useResponsiveContext,
+] = constate(useResponsive);
