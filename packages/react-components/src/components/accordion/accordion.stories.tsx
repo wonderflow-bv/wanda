661c32f4973e8128864f63c48b354e3ead308df1
Mattia <astorino.mattia@gmail.com>
2022-09-13 11:03:35 +0200

    chore: update story

--- packages/react-components/src/components/accordion/accordion.stories.tsx
+++ packages/react-components/src/components/accordion/accordion.stories.tsx
@@ -25,6 +25,35 @@ const Template: ComponentStory<typeof Accordion> = args => (
 
 export const Default = Template.bind({});
 Default.args = {
-  ratio: '1',
-  showSeparators: false,
+  showSeparators: true,
+};
+
+const NestedTemplate: ComponentStory<typeof Accordion> = args => (
+  <Accordion defaultOpen="1" {...args}>
+    <Accordion.Item value="1" summary="Item 1">
+      Item 1
+      <Accordion defaultOpen="3" {...args}>
+        <Accordion.Item value="1" summary="Item 1">
+          Item 1
+        </Accordion.Item>
+        <Accordion.Item value="2" summary="Item 2">
+          Item 2
+        </Accordion.Item>
+        <Accordion.Item value="3" summary="Item 3">
+          Item 3
+        </Accordion.Item>
+      </Accordion>
+    </Accordion.Item>
+    <Accordion.Item value="2" summary="Item 2">
+      Item 2
+    </Accordion.Item>
+    <Accordion.Item value="3" summary="Item 3">
+      Item 3
+    </Accordion.Item>
+  </Accordion>
+);
+
+export const Nested = NestedTemplate.bind({});
+Nested.args = {
+  showSeparators: true,
 };
