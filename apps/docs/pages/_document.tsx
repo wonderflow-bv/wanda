30e5f63c3acb895616a33365606fe6149e7ad60c
Mattia <astorino.mattia@gmail.com>
2022-09-23 10:02:53 +0200

    chore: add smartlook

--- apps/docs/pages/_document.tsx
+++ apps/docs/pages/_document.tsx
@@ -17,6 +17,21 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <Script
+            id="google-analytics"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              // eslint-disable-next-line @typescript-eslint/naming-convention
+              __html: `
+              window.smartlook||(function(d) {
+              var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
+              var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
+              c.charset='utf-8';c.src='https://web-sdk.smartlook.com/recorder.js';h.appendChild(c);
+              })(document);
+              smartlook('init', '8bd03bd71deed10dc7ed5241baf0c965969358c1', { region: 'eu' });
+            `,
+            }}
+          />
         </body>
       </Html>
     );
