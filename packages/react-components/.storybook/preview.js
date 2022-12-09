25b91a31f258ff095a0ae115ce67d7775b0e0f93
patrick <patrick.tabarelli@gmail.com>
2022-12-09 17:00:19 +0100

    chore: update storybook viewports values settings

--- packages/react-components/.storybook/preview.js
+++ packages/react-components/.storybook/preview.js
@@ -1,21 +1,60 @@
-import '@wonderflow/themes';
-import '../src/core.css';
-import './overrides.css';
+import "@wonderflow/themes";
+import "../src/core.css";
+import "./overrides.css";
 
 export const parameters = {
   breakpoints: {
     breakpointNames: {
-      'extra-small': '0',
-      'small': '481',
-      'medium': '769',
-      'large': '961',
-      'extra-large': '1281'
+      "extra-small": "0",
+      small: "481",
+      medium: "769",
+      large: "961",
+      "extra-large": "1281",
     },
-    debounceTimeout: 200
+    debounceTimeout: 200,
   },
-  actions: { argTypesRegex: '^on[A-Z].*' },
+  viewport: {
+    viewports: {
+      xs: {
+        name: "mobile",
+        styles: {
+          width: "480px",
+          height: "100%",
+        },
+      },
+      s: {
+        name: "tablet",
+        styles: {
+          width: "768px",
+          height: "100%",
+        },
+      },
+      m: {
+        name: "laptop",
+        styles: {
+          width: "960px",
+          height: "100%",
+        },
+      },
+      l: {
+        name: "small screen",
+        styles: {
+          width: "1280px",
+          height: "100%",
+        },
+      },
+      xl: {
+        name: "large screen",
+        styles: {
+          width: "1600px",
+          height: "100%",
+        },
+      },
+    },
+  },
+  actions: { argTypesRegex: "^on[A-Z].*" },
   previewTabs: {
-    'storybook/docs/panel': {
+    "storybook/docs/panel": {
       hidden: true,
     },
   },
@@ -27,17 +66,21 @@ export const parameters = {
   },
   themes: {
     list: [
-      { name: 'auto', color: 'linear-gradient(to bottom right, lightgray 50%, black 50.1%)' },
-      { name: 'light', color: 'lightgray' },
-      { name: 'dark', color: 'black', default: true },
+      {
+        name: "auto",
+        color: "linear-gradient(to bottom right, lightgray 50%, black 50.1%)",
+      },
+      { name: "light", color: "lightgray" },
+      { name: "dark", color: "black", default: true },
     ],
     onChange: (theme) => {
-      const iframeDocument = document.querySelector('#storybook-preview-iframe').contentDocument.documentElement;
+      const iframeDocument = document.querySelector("#storybook-preview-iframe")
+        .contentDocument.documentElement;
       if (theme) {
         iframeDocument.dataset.theme = theme.name;
       } else {
-        iframeDocument.dataset.theme = 'light';
+        iframeDocument.dataset.theme = "light";
       }
     },
-  }
+  },
 };
