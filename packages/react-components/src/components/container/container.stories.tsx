cb60a54c7509a77acb330d21f68c611ec6f14bd0
patrick <patrick.tabarelli@gmail.com>
2023-02-15 12:11:59 +0100

    feat(container): add mockup in SB

--- packages/react-components/src/components/container/container.stories.tsx
+++ packages/react-components/src/components/container/container.stories.tsx
@@ -1,5 +1,7 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import { Stack } from '@/components';
 
 import { Container } from './container';
 
@@ -64,3 +66,107 @@ export const AsSection = Template.bind({});
 AsSection.args = {
   className: 'ContainerEx',
 };
+
+const TemplateTesting: ComponentStory<typeof Container> = () => {
+  const [cols, setCols] = useState<number>(6);
+  const [wWidth, setWWidth] = useState<number>(0);
+  const [cWidth, setCWidth] = useState<number>(0);
+  const [colWidth, setColWidth] = useState<number>(0);
+  const [isLeftOpen, setIsLeftOpen] = useState<boolean>(false);
+  const [isRightOpen, setIsRightOpen] = useState<boolean>(false);
+
+  const handleResize = () => {
+    const w = window.innerWidth;
+    setWWidth(w);
+    const c = document?.querySelector('[data-container-dimension="auto"]')?.offsetWidth || 0;
+    setCWidth(c);
+    const col = document?.querySelectorAll('.col')[0]?.offsetWidth || 0;
+    setColWidth(col);
+    if (w >= 1280) setCols(6);
+    else if (w >= 768) setCols(4);
+    else setCols(2);
+  };
+
+  useEffect(
+    () => {
+      handleResize();
+      window.addEventListener('resize', handleResize);
+
+      return () => window.removeEventListener('resize', handleResize);
+    }, [],
+  );
+
+  useEffect(
+    () => handleResize(), [isLeftOpen, isRightOpen],
+  );
+
+  return (
+    <Container dimension="full" padding={false} style={{ backgroundColor: '#f9fafb' }}>
+      <Stack direction="row">
+        <Stack style={{
+          maxWidth: isLeftOpen ? '279px' : '63px',
+          backgroundColor: '#e4e7ec',
+          padding: '0.5rem',
+        }}
+        >
+          <button type="button" style={{ maxWidth: '48px', fontSize: '10px', maxHeight: '18px' }} onClick={() => setIsLeftOpen(!isLeftOpen)}>{isLeftOpen ? 'large' : 'small'}</button>
+        </Stack>
+
+        <Stack>
+          <div style={{
+            maxHeight: '64px', height: '64px', backgroundColor: '#9ea5b3', display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'flex-start', padding: '0.5rem',
+          }}
+          >
+            Window width
+            {' '}
+            {wWidth}
+            px
+            {' / '}
+            Container width
+            {' '}
+            {cWidth}
+            px
+            {' / '}
+            Column width
+            {' '}
+            {colWidth}
+            px
+            <button type="button" style={{ maxWidth: '48px', height: '18px', fontSize: '10px' }} onClick={() => setIsRightOpen(!isRightOpen)}>filters</button>
+          </div>
+
+          <Container dimension="auto" className="ContainerEx">
+            <Stack direction="row" columnGap={24}>
+              {
+            Array(cols).fill('col').map((e, i) => (
+              <div className="col" key={Math.random()} style={{ backgroundColor: 'grey', minHeight: 'calc(100vh - 64px)' }}>
+                {e}
+                {i + 1}
+              </div>
+            ))}
+
+            </Stack>
+          </Container>
+
+        </Stack>
+
+        <Stack style={{
+          maxWidth: isRightOpen ? '299px' : '0px',
+          backgroundColor: '#e4e7ec',
+          overflow: 'hidden',
+        }}
+        >
+          <div style={{
+            maxHeight: '64px', height: '64px', backgroundColor: '#e4e7ec', padding: '0.5rem',
+          }}
+          >
+            Filters
+          </div>
+        </Stack>
+
+      </Stack>
+    </Container>
+  );
+};
+
+export const Testing = TemplateTesting.bind({});
+
