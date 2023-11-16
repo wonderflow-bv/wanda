788e979d8534aae4f1ac0b6ba9ab1cae5c259b48
patrick <patrick.tabarelli@gmail.com>
2023-11-16 18:14:39 +0100

    refactor(charts)

--- packages/charts/src/utils/colors.ts
+++ packages/charts/src/utils/colors.ts
@@ -22,22 +22,10 @@ export const getCartesianStyleConfigFromTheme = (theme: 'light' | 'dark') => {
   const cStyle = _.cloneDeep(cartesianStyleConfig);
 
   if (theme === 'dark') {
-    cStyle.linearGradient.background.from = themeDark.background.from;
-    cStyle.linearGradient.background.to = themeDark.background.to;
-
     cStyle.axis.axisLineProps.stroke = themeDark.axis.line;
     cStyle.axis.labelProps.fill = themeDark.axis.label;
     cStyle.axis.tickLabelProps.fill = themeDark.axis.tickLabel;
     cStyle.axis.tickLineProps.stroke = themeDark.axis.tick;
-
-    cStyle.grid.rows!.stroke = themeDark.grid.line;
-    cStyle.grid.columns!.stroke = themeDark.grid.line;
-
-    cStyle.headings.title.fill = themeDark.headings.title;
-    cStyle.headings.subtitle.fill = themeDark.headings.subtitle;
-
-    cStyle.grid.background.from = themeDark.grid.background.from;
-    cStyle.grid.background.to = themeDark.grid.background.to;
   }
 
   return cStyle;
