8a21a6fb3caae5641e2a77e33a69bd3d58cf9e6b
patrick <patrick.tabarelli@gmail.com>
2023-12-21 10:24:11 +0100

    docs(charts): initial sb

---
+++ packages/react-components/src/examples/charts/storybook/bar-chart.stories.tsx
@@ -0,0 +1,55 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+// import { BarChart, BarChartProps, CartesianChartLayout } from '@wonderflow/charts';
+import { BarChart, BarChartProps, CartesianChartLayout } from '../../../../../charts/src';
+import {
+  feedbackCount,
+} from '../mock-data';
+
+const story: ComponentMeta<typeof BarChart> = {
+  title: 'Charts/Bar Chart',
+  component: BarChart,
+  args: {
+    title: 'Bar Charts',
+    subtitle: 'A quantitative bar chart',
+    isStacked: false,
+    sortBy: 'dataKey',
+    preventResponsive: false,
+    isLoading: false,
+    hideLegend: false,
+    hidePadding: false,
+    layout: CartesianChartLayout.HORIZONTAL,
+    theme: 'light',
+    data: feedbackCount,
+    index: {
+      dataKey: 'date',
+      label: 'Year',
+    },
+    series: {
+      dataKey: ['feedback count'],
+      label: 'Feedback Count',
+    },
+    overlay: undefined,
+  },
+  argTypes: {
+    layout: {
+      options: ['horizontal', 'vertical'],
+      control: { type: 'radio' },
+    },
+    sortBy: {
+      options: ['dataKey', 'dataKey-reverse', 'value', 'value-reverse'],
+      control: { type: 'select' },
+    },
+    theme: {
+      options: ['light', 'dark'],
+      control: { type: 'radio' },
+    },
+  },
+};
+
+export default story;
+
+const Template: ComponentStory<typeof BarChart> = (args: BarChartProps) => <BarChart {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {};
