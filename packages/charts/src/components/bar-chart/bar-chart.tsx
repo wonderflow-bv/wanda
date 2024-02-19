203c551c2a229dcfa23bdf44cbf4b84579a08e32
patrick <patrick.tabarelli@gmail.com>
2024-02-19 16:54:08 +0100

    feat(charts): initial barchart

--- packages/charts/src/components/bar-chart/bar-chart.tsx
+++ packages/charts/src/components/bar-chart/bar-chart.tsx
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 import { Except } from 'type-fest';
 
 import {
@@ -30,6 +30,8 @@ import {
   BarChartSeries, BarChartTooltip,
 } from '../../types/bar-chart';
 import {
+  computeAverage,
+  computeTrendline,
   handleBarChartSeriesColors,
   handleChartAxisLayout,
   handleChartDomainAndScaleType,
@@ -51,7 +53,7 @@ export type BarChartProps = {
    */
   data: Data;
   /**
-   * If true bars get stacked one upon each other.
+   * If true bars get stacked one upon the other.
    */
   isStacked?: boolean;
   /**
@@ -76,9 +78,25 @@ export type BarChartProps = {
    */
   overlay?: BarChartSeries;
   /**
+   * Display an average line and label when `true`.
+   */
+  showAverage?: boolean;
+  /**
+   * Display a trend line for every single set of bars when `true`.
+   */
+  showTrendline?: boolean;
+  /**
    * Set extra data or custom content to be displayed in the tooltip.
    */
   tooltip?: BarChartTooltip;
+  /**
+   * Prevent from displaying the tooltip on the chart when `true`.
+   */
+  preventTooltipDisplay?: boolean;
+  /**
+   * Show a label for every single bar.
+   */
+  showLabel?: boolean;
   /**
    * Remove the padding from the chart container.
    */
@@ -95,18 +113,33 @@ export const BarChart: React.FC<BarChartProps> = ({
   series,
   overlay,
   tooltip,
+  preventTooltipDisplay = false,
+  showAverage = false,
+  showTrendline = false,
+  showLabel,
   hidePadding = false,
   ...otherProps
 }: BarChartProps) => {
+  const [brushFilteredData, setBrushFilteredData] = useState<Data>(data);
+
   const isHorizontal = layout === CartesianChartLayout.HORIZONTAL;
 
   const { index: i, series: s, overlay: o } = useMemo(
     () => handleChartDomainAndScaleType(data, index, series, overlay), [data, index, overlay, series],
   );
 
+  const { index: iFiltered, series: sFiltered, overlay: oFiltered } = useMemo(
+    () => handleChartDomainAndScaleType(brushFilteredData, index, series, overlay),
+    [brushFilteredData, index, overlay, series],
+  );
+
   const axis = useMemo(() => handleChartAxisLayout(i, s, o), [i, s, o]);
 
-  const palette = useMemo(() => colorPaletteDefault[theme], [theme]);
+  const axisFiltered = useMemo(() => handleChartAxisLayout(iFiltered, sFiltered, oFiltered),
+    [iFiltered, sFiltered, oFiltered]);
+
+  const paletteSeries = colorPaletteDefault[theme];
+  const paletteOverlay = useMemo(() => [...paletteSeries.slice(6), ...paletteSeries.slice(0, 6)], [paletteSeries]);
 
   const zeroPadding: MarginProps | undefined = hidePadding ? {
     top: 0, right: 12, bottom: 0, left: 0,
@@ -120,33 +153,55 @@ export const BarChart: React.FC<BarChartProps> = ({
     series: {
       dataKey: series.dataKey,
       names: handleSeriesNames(series),
-      colors: handleBarChartSeriesColors(series, palette),
+      colors: handleBarChartSeriesColors(series, paletteSeries),
       style: series.style,
+      average: showAverage ? computeAverage(data, series.dataKey) : undefined,
+      trendline: showTrendline ? computeTrendline(data, series.dataKey) : undefined,
     },
     overlay: {
       dataKey: overlay?.dataKey,
       names: overlay ? handleSeriesNames(overlay) : undefined,
-      colors: overlay ? handleBarChartSeriesColors(overlay, palette.reverse()) : undefined,
+      colors: overlay ? handleBarChartSeriesColors(overlay, paletteOverlay) : undefined,
       style: overlay?.style,
+      average: (overlay?.dataKey && showAverage) ? computeAverage(data, overlay.dataKey) : undefined,
+      trendline: (overlay?.dataKey && showTrendline) ? computeTrendline(data, overlay.dataKey) : undefined,
     },
     tooltip,
+    preventTooltipDisplay,
+    showAverage,
+    showTrendline,
+    showLabel,
     hidePadding,
-  }), [isStacked, sortBy, index.dataKey, series, palette, overlay, tooltip, hidePadding]);
+  }), [
+    isStacked,
+    sortBy,
+    index.dataKey,
+    series,
+    paletteSeries,
+    showAverage,
+    data,
+    showTrendline,
+    overlay,
+    paletteOverlay,
+    tooltip,
+    preventTooltipDisplay,
+    showLabel,
+    hidePadding]);
 
   return (
     <ThemeProvider theme={theme}>
       <LayoutProvider layout={layout}>
-        <DataProvider data={data} metadata={metadata} filteredData={data}>
+        <DataProvider data={data} metadata={metadata} filteredData={brushFilteredData}>
           <CartesianBase
             {...otherProps}
             axis={axis[layout]}
-            axisFiltered={axis[layout]}
+            axisFiltered={axisFiltered[layout]}
             grid={{
               hideRows: !isHorizontal,
               hideColumns: isHorizontal,
             }}
             margin={zeroPadding}
-            onBrushChange={() => ({})}
+            onBrushChange={setBrushFilteredData}
           />
         </DataProvider>
       </LayoutProvider>
