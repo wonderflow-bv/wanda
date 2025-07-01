6aba27af74d41b1f3fae7eb3bb915da13746f215
Andrea Pavia <andrea.pavia@wonderflow.ai>
2025-07-01 14:54:06 +0200

    fix(product-card): fix product card cover images loading when source is missing

--- packages/react-components/src/components/product-card/product-card-media/product-card-media.tsx
+++ packages/react-components/src/components/product-card/product-card-media/product-card-media.tsx
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 Wonderflow Design Team
+ * Copyright 2023-2025 Wonderflow Design Team
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -92,7 +92,7 @@ export const ProductCardMedia = forwardRef(({
 
   useEffect(() => {
     void preloadImages(source);
-  }, [preloadImages, source]);
+  }, [source, preloadImages]);
 
   if (source.length === 0) return null;
 
